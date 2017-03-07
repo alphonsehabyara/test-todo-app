bd28c8538e536e3b44b1e09d8a2a5daef664b028
Alphonse Habyarimana <habyara.malpha@gmail.com>
2017-03-07 10:37:53 +0200

    Finished testing

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -14,4 +14,48 @@ RSpec.describe Task, type: :model do
       expect(task.complete).to eq(true)
     end
   end
+  describe '#toggle_favorite!' do 
+    it "should switch favorite to false if it began as true" do
+      task = Task.new(favorite: true)
+      task.toggle_favorite!
+      expect(task.favorite).to eq(false)
+    end
+    it "should switch favorite to true if it began as false" do 
+      task = Task.new(favorite: false)
+      task.toggle_favorite!
+      expect(task.favorite).to eq(true)
+    end
+  end
+   describe '#overdue?' do 
+    it "should false if the deadline didn't pass" do
+      task = Task.new(deadline: Time.now - 1.day)
+      expect(task.overdue?).to eq(true)
+    end
+
+    it "should true if the deadline passed" do 
+      task = Task.new(deadline: Time.now + 1.day)
+      expect(task.overdue?).to eq(false)
+    end
+  end 
+  describe 'increment_priority!' do
+    it 'should return the priority increased by one' do
+      task = Task.new(priority: 15)
+      task.increment_priority!
+      expect(task.priority).to eq(16)
+    end
+  end
+  describe 'decrement_priority!' do
+    it 'should return the priority increased by one' do
+      task = Task.new(priority: 15)
+      task.decrement_priority!
+      expect(task.priority).to eq(14)
+    end
+  end
+  describe 'snooze_hour!' do
+    it 'should deadline plus one hour' do
+      task = Task.new(deadline: Time.now )
+      task.snooze_hour!
+      expect(task.deadline).to eq(Time.now)
+    end
+  end
 end
